04e9c9bfaafe80df5494d69d91076442b952d241
Hannah Drury <hannah_drury@yahoo.com>
2019-12-07 10:02:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,13 +14,23 @@ function whisper(string)
 
 function logShout(string)
 {
-  return console.log(string.toUpperCase);
+ console.log(string.toUpperCase)
 }
 
 function logWhisper(string)
 {
-  return console.log(string.toLowerCase);
+ console.log(string.toLowerCase)
 }
+
+function sayHiToGrandma(string)
+{
+  if (string === toLowerCase)
+  {
+    return 'I can\'t hear you'
+  }
+}
+
+
 
 logWhisper(string);
 logShout(string);
