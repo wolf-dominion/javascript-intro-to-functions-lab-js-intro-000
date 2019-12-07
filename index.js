b921a72926741a6e1360288a0542c28e9dd48468
Hannah Drury <hannah_drury@yahoo.com>
2019-12-07 10:13:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ function sayHiToGrandma(string)
   {
     return "YES INDEED!"
   }
-  if (string === "I love you, Grandma")
+  if (string === "I love you, Grandma.")
   {
     return 'I love you too.'
   }
