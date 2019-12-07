8cb67819559331bec06954765e0c69f54b0b2398
Hannah Drury <hannah_drury@yahoo.com>
2019-12-07 10:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,7 +26,15 @@ function sayHiToGrandma(string)
 {
   if (string === string.toLowerCase)
   {
-    return "I can't hear you"
+    return "I can't hear you!"
+  }
+  if (string === string.toUpperCase)
+  {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma")
+  {
+    return 'I love you too.'
   }
 }
 
